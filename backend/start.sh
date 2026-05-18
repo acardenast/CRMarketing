f0b0e0548ee8581cd2a0792786ea007c70bbc8e4
Antonio Cardenas <cardenastri14@gmail.com>
2026-05-18 17:26:59 +0200

    fix start y dockerfile

--- backend/start.sh
+++ backend/start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd /var/www/html
 composer install --no-dev --optimize-autoloader
+touch /var/www/html/database/database.sqlite
 php artisan key:generate --force
 php artisan migrate:fresh --force
 php artisan db:seed --force 2>/dev/null || true
