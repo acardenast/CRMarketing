1c472d230b42e05c5c82059bcf93445163c3784b
Antonio Cardenas <cardenastri14@gmail.com>
2026-05-18 17:15:35 +0200

    fix migrate fresh

--- backend/start.sh
+++ backend/start.sh
@@ -2,7 +2,7 @@
 cd /var/www/html
 composer install --no-dev --optimize-autoloader
 php artisan key:generate --force
-php artisan migrate --force
+php artisan migrate:fresh --force
 php artisan db:seed --force 2>/dev/null || true
 php artisan config:cache
 php artisan route:cache
