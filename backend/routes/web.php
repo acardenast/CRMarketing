a1a34149ae476e1930ac6dd0c733ffc67b1e1165
Antonio Cardenas <cardenastri14@gmail.com>
2026-05-19 17:22:08 +0200

    test routes

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,4 +1,10 @@
 <?php
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {return view('welcome');});
+Route::get('/', function () {
+    return redirect('/login');
+});
+
+Route::get('/test', function () {
+    return 'laravel ok';
+});
