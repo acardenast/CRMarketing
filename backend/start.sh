f7b9479719e1c1da74b3cbfed078a86603900dad
Antonio Cardenas <cardenastri14@gmail.com>
2026-05-22 17:07:37 +0200

    fix: prevent exit on artisan errors

--- backend/start.sh
+++ backend/start.sh
@@ -8,8 +8,8 @@ composer install --no-dev --optimize-autoloader
 chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
 chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
 
-php artisan optimize:clear
-php artisan migrate --force
+php artisan optimize:clear || true
+php artisan migrate --force || true
 php artisan db:seed --force || true
 php artisan config:cache
 php artisan route:cache
