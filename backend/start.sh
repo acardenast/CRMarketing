fbd32e341a110623d5baa9ac5189fb8ac722b876
Antonio Cardenas <cardenastri14@gmail.com>
2026-05-22 17:39:24 +0200

    Arreglar archivo .sh

--- backend/start.sh
+++ backend/start.sh
@@ -9,8 +9,12 @@ chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
 chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
 
 php artisan optimize:clear || true
+php artisan config:clear || true
+php artisan cache:clear || true
+
 php artisan migrate --force || true
 php artisan db:seed --force || true
+
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
